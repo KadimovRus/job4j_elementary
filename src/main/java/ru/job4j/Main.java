a2b1469316742cf7274d7747a356745bf28dca14
Rustam Kadimov <kadim_r@mail.ru>
2023-08-22 11:44:54 +0500

    4.7. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("  Привет мир 123!   ");
-        System.out.println("  Привет мир 2345!   ");
+        System.out.println("  Привет мир 123546!   ");
+        System.out.println("  Привет мир 2345455!   ");
     }
 }
